4390754dc976e77438d5b1ef9087cd5ac7ae4523
Lowell Fortune <lowellfortune@gmail.com>
2016-12-02 17:43:40 -0600

    droped statuses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201193318) do
+ActiveRecord::Schema.define(version: 20161202233945) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -108,12 +108,6 @@ ActiveRecord::Schema.define(version: 20161201193318) do
     t.string   "role_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "statuses", force: :cascade do |t|
-    t.string   "name",       default: "f"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade do |t|
