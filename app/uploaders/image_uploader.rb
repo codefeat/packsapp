54260f6493fab457cd8f4a4d83ccc5791ae599dd
Lowell Fortune <lowellfortune@gmail.com>
2016-12-06 03:47:31 -0600

    cleand up delivery and pickup pgs

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,6 +19,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("/images/fallback/" + [version_name, "default-profile.png"].compact.join('_'))
+  end
+
+  def default_url
+    'default-profile.png'
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
