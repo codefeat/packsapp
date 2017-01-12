f3d7d69c68509182c601321ca0b4271c2be71662
Lowell Fortune <lowellfortune@gmail.com>
2017-01-11 20:56:23 -0600

    updated stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.secrets.publishable_key,
-  :secret_key      => Rails.application.secrets.secret_key
+  # :publishable_key => Rails.application.secrets.publishable_key,
+  # :secret_key      => Rails.application.secrets.secret_key
+  :publishable_key => pk_test_87LayhWLhTKpfeY88G6tdHAb,
+  :secret_key      => sk_test_hhnZgxUzpid4PweuND5kNXXE
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
