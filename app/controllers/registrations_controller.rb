29385633047504ab91b17db49f9d254115f01aa4
Lowell Fortune <lowellfortune@gmail.com>
2017-01-19 10:16:08 -0600

    added show to reg controller for email confirm

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,19 @@ class RegistrationsController < Devise::RegistrationsController
   #   end
   # end
 
+def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with_navigational(resource){ redirect_to
+            after_confirmation_path_for(deliveries, resource) }
+    else
+      flash[:notice] = "Your account is already confirmed. Please login."
+      redirect_to '/login'
+    end
+  end
 
   def confirmation_token
         if self.confirmation_token.blank?
