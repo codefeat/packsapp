c1a70ef2ac0a2703f4b41fa2dc718c129822bd6b
codefeat <lowellfortune@gmail.com>
2017-06-21 22:35:31 -0500

    commented out trailing ends in migration

--- db/migrate/20170621165636_alter_column_zip_id_in_users.rb
+++ db/migrate/20170621165636_alter_column_zip_id_in_users.rb
@@ -2,11 +2,11 @@ class AlterColumnZipIdInUsers < ActiveRecord::Migration
   def self.up
     #change_table :users do |t|
       #t.change :zip_id, :string
-  end
+  #end
 end
   def self.down
     #change_table :users do |t|
       #t.change :zip_id, :integer
-  end
+  #end
  end
 end
