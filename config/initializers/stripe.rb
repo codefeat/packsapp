9e56530a05c09dbad2ea5e53ee8111f35cd639f7
Lowell Fortune <lowellfortune@gmail.com>
2017-10-04 03:48:02 -0500

    updated vars for stripe live

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,11 @@
 Rails.configuration.stripe = {
   # :publishable_key => Rails.application.secrets.publishable_key,
   # :secret_key      => Rails.application.secrets.secret_key
-  :publishable_key => 'pk_test_87LayhWLhTKpfeY88G6tdHAb',
-  :secret_key      => 'sk_test_hhnZgxUzpid4PweuND5kNXXE'
+  #:publishable_key => 'pk_test_87LayhWLhTKpfeY88G6tdHAb',
+  #:secret_key      => 'sk_test_hhnZgxUzpid4PweuND5kNXXE'
+
+  :stripe_publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :stripe_secret_key => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
