aa4769fa065b2d5e7483a00671b3f8d3b63980d8
Lowell Fortune <lowellfortune@gmail.com>
2017-10-30 05:19:40 -0500

    send email when order is created

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Order do
-
+after_create do |order|
+	PurchaseMailer.new_order(@order).deliver_now
+end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
