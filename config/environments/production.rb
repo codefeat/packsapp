fbe9db60b3fbea2d4f312e90f014cb0fd490235d
Lowell Fortune <lowellfortune@gmail.com>
2017-10-31 20:52:11 -0500

    updated prod vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port       => 25,
-    :authentication => :plain,
+    :port       => 587,
+    :authentication => 'plain',
     :user_name    => ENV['SENDGRID_USERNAME'],
     :password   => ENV['SENDGRID_PASSWORD'],
     :domain   =>  'heroku.com',
